f2ef65fec7740919323c3caff979d5f2cf5fe7e1
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2019-02-10 17:14:45 -0600

    Update Assignment_1.py

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -13,11 +13,18 @@ import csv
 #from sklearn import tree, metrics
 
 #reader = csv.reader(open(r'C:\Users\XXZ180012\Desktop\Assignment_1\training_set.csv', "r"))
-#for row in reader:
-#    print(row)
+#
+#variables = reader
 
 binary = pd.read_csv(r'C:\Users\XXZ180012\Desktop\Assignment_1\training_set.csv')
-        
+
+def entropy(data, member):
+    entropy = 0
+    values = data[member].unique()
+    for value in values:
+        fraction =data[member].value_counts()[value]/len(data[member])
+        entropy += -(fraction * np.log2(fraction))
+    return entropy
     
 def test_split(index, value, dataset):
 	left, right = list(), list()
