7cd113fa331b5a91df42152475ee4a3586f4fd50
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2019-02-10 16:29:41 -0600

    Update Assignment_1.py

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -4,10 +4,26 @@ Created on Fri Feb  8 13:49:33 2019
 
 @author: XXZ180012
 """
-
+import os
+import numpy as np
+import pandas as pd
+import numpy as np, pandas as pd
+import matplotlib.pyplot as plt
 import csv
+#from sklearn import tree, metrics
 
-with open ("C:\Users\Desktop\\training_set.csv","r") as csv_file:
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-        print(row)
+#reader = csv.reader(open(r'C:\Users\XXZ180012\Desktop\Assignment_1\training_set.csv', "r"))
+#for row in reader:
+#    print(row)
+
+binary = pd.read_csv(r'C:\Users\XXZ180012\Desktop\Assignment_1\training_set.csv')
+        
+    
+def test_split(index, value, dataset):
+	left, right = list(), list()
+	for row in dataset:
+		if row[index] < value:
+			left.append(row)
+		else:
+			right.append(row)
+	return left, right
