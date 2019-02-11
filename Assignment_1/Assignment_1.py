43bb8c63a6fcf05fa33ab5f8b84abcc67119a460
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2019-02-11 11:07:07 -0600

    Update Assignment_1.py

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -17,23 +17,34 @@ import csv
 
 binary = pd.read_csv(r'C:\Users\XXZ180012\Desktop\Assignment_1\training_set.csv')
 
-def entropy(data, member):
+def entropy(data):
     entropy = 0
-    values = data[member].unique()
+    values = data["Class"].unique()
     for value in values:
-        fraction =data[member].value_counts()[value]/len(data[member])
+        fraction =data["Class"].value_counts()[value]/len(data["Class"])
         entropy += -(fraction * np.log2(fraction))
     return entropy
     
 
 def information_Gain(data, attribute):
-    data_1 = data[data[attribute] == 1]
-    data_0 = data[data[attribute] == 0]
-    print(data_1)
-    print(data_0)
+    total_entropy = entropy(data)
+    values = data[attribute].unique()
+    for value in values:
+        data_value = data[data[attribute] == value]
+        total_entropy -= len(data_value)*entropy(data_value)/len(data)
+    return total_entropy
+
+
+def buildNode(data):
+    
+
+#    print(data_1)
+#    print(data_0)
     
     
-    
+#for attribute in list(binary):
+#    if attribute != "Class":
+#        print("the information gain for " + str(attribute) + "=", information_Gain(binary, attribute))        
     
     
 
