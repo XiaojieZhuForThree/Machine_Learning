a7c38c36be544db8e95b93a1f0a34c9ae7fbdfd8
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2019-02-15 11:59:47 -0600

    Update Assignment_1.py

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -16,35 +16,22 @@ import pandas as pd
 #
 #variables = reader
 
-binary = pd.read_csv(r'C:\Users\XXZ180012\Desktop\Assignment_1\data_sets1\training_set.csv')
+trainSet1 = pd.read_csv(r'C:\Users\XXZ180012\Desktop\Assignment_1\data_sets1\training_set.csv')
+trainSet2 = pd.read_csv(r'C:\Users\XXZ180012\Desktop\Assignment_1\data_sets2\training_set.csv')
 
-#class decisionTree:
-#    def __init__(self, data):
-#        self.data = data
-#        self.spliter = None
-#        
-#    def buildTree():
-#        maxGain = 0;
-#        for attribute in set(data):
-#            if information_Gain(data, attribute) > maxGain:
-#                maxGain = information_Gain(data, attribute)
-#                spliter = attribute
-#        root = spliter
-#        root.right, root.left = buildNode(data, spliter)
-#        
-#        
-#        
-#        
-
-class ParentNode:
+class TreeNode:
     def __init__(self, name):
         self.name = name
         self.pos = None
         self.neg = None
 
-class LeafNode:
-    def __init__(self, value):
-        self.value = value
+    def __str__(self):
+        printTree(self)
+        return ""
+    
+    class LeafNode:
+        def __init__(self, value):
+            self.value = value
 
 def entropy(data):
     entropy = 0
@@ -53,32 +40,6 @@ def entropy(data):
         fraction =data["Class"].value_counts()[value]/len(data["Class"])
         entropy += -(fraction * np.log2(fraction))
     return entropy
-    
-
-def information_Gain(data, attribute):
-    total_entropy = entropy(data)
-    values = data[attribute].unique()
-    for value in values:
-        data_value = data[data[attribute] == value]
-        total_entropy -= len(data_value)*entropy(data_value)/len(data)
-    return total_entropy
-
-def findSplit(data, used):
-    check = set(data) - used
-    maxGain = None
-    maxVal = 0
-    for attribute in check:
-        iG = information_Gain(data, attribute)
-        if iG > maxVal:
-            maxVal = iG
-            maxGain = attribute
-    return maxGain
-
-
-def buildNode(data, attribute):
-    subdata_1 = data[data[attribute] == 1]
-    subdata_0 = data[data[attribute] == 0]
-    return subdata_1, subdata_0
 
 def varImpurity(data):
     part1 = 0
@@ -90,79 +51,127 @@ def varImpurity(data):
         part2 = data["Class"].value_counts()[1]/len(data)
     return part1 * part2
 
-def impurityGain(data, attribute):
+def impurity_Gain(data, attribute):
     VI = varImpurity(data)
     values = data[attribute].unique()
     for value in values:
         data_value = data[data[attribute] == value]
         VI -= len(data_value)*varImpurity(data_value)/len(data)
-    return VI
+    return VI    
 
-def findSplit2(data, used):
+def information_Gain(data, attribute):
+    total_entropy = entropy(data)
+    values = data[attribute].unique()
+    for value in values:
+        data_value = data[data[attribute] == value]
+        total_entropy -= len(data_value)*entropy(data_value)/len(data)
+    return total_entropy
+
+def findSplit(data, used, method):
     check = set(data) - used
+    if len(check) == 0:
+        return None
     maxGain = None
-    maxVal = 0
+    maxVal = float('-inf')
     for attribute in check:
-        iG = impurityGain(data, attribute)
-        if iG > maxVal:
-            maxVal = iG
+        gain = method(data, attribute)
+        if gain > maxVal:
+            maxVal = gain
             maxGain = attribute
     return maxGain
 
-def buildTree(data, used):
+def split(data, attribute):
+    subdata_1 = data[data[attribute] == 1]
+    subdata_0 = data[data[attribute] == 0]
+    return subdata_1, subdata_0
+
+
+def buildTree(data, method, used = set(["Class"])):
     if len(data["Class"].unique()) == 1:
-        return LeafNode(data['Class'][0])
+        return TreeNode.LeafNode(data['Class'].unique()[0])
     else: 
-        rootVal = findSplit(data, used)
+        rootVal = findSplit(data, used, method)
         if rootVal is not None:
             used.add(rootVal)
-            leftSet, rightSet = buildNode(data, rootVal)
-            root = ParentNode(rootVal)
-            root.pos = buildTree(leftSet, set(used))
-            root.neg = buildTree(rightSet, set(used))
+            leftSet, rightSet = split(data, rootVal)
+            root = TreeNode(rootVal)
+            root.pos = buildTree(leftSet, method, set(used))
+            root.neg = buildTree(rightSet, method, set(used))
             return root
         return None
-def printTree(root, n = 0):
-    if (type(root) == LeafNode):
-        print(root.value)
-        return
-    else:
-        print("\n" + "| " * n + root.name +  " = 1: ", end = '')
-        printTree(root.pos, n+1)
-        print("\n" + "| " * n + root.name +  " = 0: ", end = '')
-        printTree(root.neg, n+1)
-        return
-def buildTree_1(data, used, n):
-    if len(data["Class"].unique()) == 1:
-        print(data["Class"].unique()[0], end = '')
-        return
-    else: 
-        rootVal = findSplit(data, used)
-        if rootVal is not None:
-            used.add(rootVal)
-            leftSet, rightSet = buildNode(data, rootVal)
-            print("\n" + "| " * n + rootVal +  " = 1: ", end = '')
-            buildTree_1(leftSet, set(used), n+1)
-            print("\n" + "| " * n + rootVal + " = 0: ", end = '')
-            buildTree_1(rightSet, set(used), n+1)
-            return
-        return
     
-def buildTree_2(data, used, n):
-    if len(data["Class"].unique()) == 1:
-        print(data["Class"].unique()[0], end = '')
-        return
-    else: 
-        rootVal = findSplit2(data, used)
-        if rootVal is not None:
-            used.add(rootVal)
-            leftSet, rightSet = buildNode(data, rootVal)
-            print("\n" + "| " * n + rootVal +  " = 1: ", end = '')
-            buildTree_2(leftSet, set(used), n+1)
-            print("\n" + "| " * n + rootVal + " = 0: ", end = '')
-            buildTree_2(rightSet, set(used), n+1)
-            return
-        return
+def printTree(root, n = 0):
+    if (type(root) == TreeNode.LeafNode):
+        print(root.value, end = '')
+    elif (type(root) == None):
+        print('None', end = "")
+    else:
+        print("\n" + "| " * n + root.name +  " = 1 : ", end = '')
+        printTree(root.pos, n+1)
+        print("\n" + "| " * n + root.name +  " = 0 : ", end = '')
+        printTree(root.neg, n+1)
+
+
+root1 = buildTree(trainSet1, information_Gain)
+root2 = buildTree(trainSet2, information_Gain)
+print(root2)
+root3 = buildTree(trainSet1, impurity_Gain)
+root4 = buildTree(trainSet2, impurity_Gain)
+
+
+#def findSplit2(data, used):
+#    check = set(data) - used
+#    maxGain = None
+#    maxVal = 0
+#    for attribute in check:
+#        iG = impurityGain(data, attribute)
+#        if iG > maxVal:
+#            maxVal = iG
+#            maxGain = attribute
+#    return maxGain       
+    
+#def buildTree_1(data, used, n):
+#    if len(data["Class"].unique()) == 1:
+#        print(data["Class"].unique()[0], end = '')
+#        return
+#    else: 
+#        rootVal = findSplit(data, used)
+#        if rootVal is not None:
+#            used.add(rootVal)
+#            leftSet, rightSet = buildNode(data, rootVal)
+#            print("\n" + "| " * n + rootVal +  " = 1: ", end = '')
+#            buildTree_1(leftSet, set(used), n+1)
+#            print("\n" + "| " * n + rootVal + " = 0: ", end = '')
+#            buildTree_1(rightSet, set(used), n+1)
+#            return
+#        return
+#    
+#def buildTree_2(data, used, n):
+#    if len(data["Class"].unique()) == 1:
+#        print(data["Class"].unique()[0], end = '')
+#        return
+#    else: 
+#        rootVal = findSplit2(data, used)
+#        if rootVal is not None:
+#            used.add(rootVal)
+#            leftSet, rightSet = buildNode(data, rootVal)
+#            print("\n" + "| " * n + rootVal +  " = 1: ", end = '')
+#            buildTree_2(leftSet, set(used), n+1)
+#            print("\n" + "| " * n + rootVal + " = 0: ", end = '')
+#            buildTree_2(rightSet, set(used), n+1)
+#            return
+#        return
+
+
+
+#initialSet1 = set(["Class"])   
+#initialSet2 = set(['Class'])
+#print("First Tree\n")
+#buildTree_1(binary, initialSet1, 0)
+#print("\nSecond Tree")
+#buildTree_2(binary, initialSet2, 0)
+#binary[((binary["XK"] == 1) & (binary["XD"] == 1) & (binary["XS"] == 1) & (binary["XP"] == 1))]
+
 
 def post_Pruning(decisionTree, L, K):
     bestOne = decisionTree
@@ -180,18 +189,6 @@ def post_Pruning(decisionTree, L, K):
         if (accuracy(newTree) > accuracy(bestOne)):
            bestOne = newTree
     return bestOne
-
-initialSet1 = set(["Class"])   
-initialSet2 = set(['Class'])
-print("First Tree\n")
-buildTree_1(binary, initialSet1, 0)
-print("\nSecond Tree")
-buildTree_2(binary, initialSet2, 0)
-binary[((binary["XK"] == 1) & (binary["XD"] == 1) & (binary["XS"] == 1) & (binary["XP"] == 1))]
-
-
-
-
 
 #    print(data_1)
 #    print(data_0)
