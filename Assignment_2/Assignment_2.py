2fddeac7fa7db23f41fe8cdcc444d332324bd15d
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2019-03-12 14:42:20 -0500

    Add files via upload

---
+++ Assignment_2/Assignment_2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 13:51:15 2019
+
+@author: xxz180012
+"""
+import numpy as np
+spam = {}
+ham = {}
+def trainMultiNomialNB(document, classes):
+    
