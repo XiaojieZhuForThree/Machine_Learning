3d650d5225e1a4e57a9687b0fa14c6a05e22b444
XiaojieZhuForThree <43705383+XiaojieZhuForThree@users.noreply.github.com>
2019-03-18 18:43:53 -0500

    Update Assignment_2.py

--- Assignment_2/Assignment_2.py
+++ Assignment_2/Assignment_2.py
@@ -5,9 +5,71 @@ Created on Tue Mar 12 13:51:15 2019
 @author: xxz180012
 """
 import numpy as np
+import pandas as pd
+import os
+
 spam = {}
 ham = {}
-def trainMultinomialNB(spam, ham, classes):
-    # what kind of values do you want to return?
-    for word in spam:
+
+hamAddr = 'C:\Users\zxj\Desktop\test\ham' 
+spamAddr = 'C:\Users\zxj\Desktop\test\spam'
+root = r'C:\Users\zxj\Desktop\test'   
+
+classes = {'ham', 'spam'}
+
+def extraVocabulary(folder):
+    vocab = set()
+    files = os.listdir(folder)
+    for file in files:
+        with open(folder + '/' + file, 'r', encoding='utf-8', errors='ignore') as doc:
+            temp = doc.read().lower().replace('\n', ' ').split(' ')
+            for word in temp:
+                vocab.add(word)
+    return vocab
+    
         
+def countVocabulary(folder, dic):
+    files = os.listdir(folder)
+    
+    for file in files:
+        with open(folder + '/' + file, 'r', encoding='utf-8', errors='ignore') as doc:
+            temp = doc.read().lower().replace('\n', ' ').split(' ')
+            for word in temp:
+                if word not in dic:
+                    dic[word] = 1
+                dic[word] += 1
+    return
+#    return len(files)
+
+def countDocs(folder):
+    files = os.listdir(folder)
+    return len(files)
+
+def countTokensOfTerm(text, v):
+    if v in text:
+        return text[v]
+    return 0
+
+extraVocabulary(r'C:\Users\zxj\Desktop\test\ham', ham)       
+extraVocabulary(r'C:\Users\zxj\Desktop\test\spam', spam) 
+
+def trainMultinomialNB(classes, documents):
+    prior = {}
+    V = extraVocabulary(hamAddr)
+    N = countDocs(hamAddr) + countDocs(spamAddr)
+    condprob = {}
+    for cls in classes:
+        N_cls = countDocs(root + "'\'" + cls)
+        prior[cls] = N_cls / N
+        for t in V:
+            condprob[t] = {}
+            Tct = countTokensOfTerm(text, t)
+            condprob[t][cls] = (Tct + 1) / sum(text[i] + 1 for i in text)
+    return V, prior, condprob        
+    
+    
+    
+    
+    
+    
+    
